3483bec06eca888d7a019fa5a184bf193033262f
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2021-11-26 00:20:28 +0600

    Create Main.java

---
+++ Strongly Connected/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int v = scanner.nextInt();
+        int e = scanner.nextInt();
+        scanner.nextLine();
+
+        Graph graph = new Graph(v);
+
+        for(int i = 1; i <= e; i++){
+            String[] input = scanner.nextLine().split(" ");
+            int s1 = Integer.parseInt(input[0]);
+            int s2 = Integer.parseInt(input[1]);
+
+            graph.addEdge(s1, s2);
+        }
+
+        if(graph.isStronglyConnected(0)) System.out.println("Strongly Connected...");
+        else System.out.println("Not Strongly Connected...");
+    }
+}
