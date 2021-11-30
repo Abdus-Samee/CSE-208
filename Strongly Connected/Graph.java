572401cd0bbbd0a68eafd04e5e151a13ba95967b
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2021-11-30 18:45:24 +0600

    Update Graph.java

--- Strongly Connected/Graph.java
+++ Strongly Connected/Graph.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Graph {
     private int n;
     private List<ArrayList<Integer>> adjacencyList;
+    private ArrayList<ArrayList<Integer>> reverseAdjList;
+    private Stack<Integer> myStack;
     private int[] visited;
     private int[] colour;
 
     public Graph(int size){
         this.n = size;
         this.adjacencyList = new ArrayList<>();
+        this.myStack = new Stack<>();
         this.visited = new int[size];
         this.colour = new int[size];
 
@@ -50,7 +54,7 @@ public class Graph {
 
         if(checkVisit()){
             this.visited = new int[this.n];
-            ArrayList<ArrayList<Integer>> reverseAdjList = new ArrayList<>();
+            this.reverseAdjList = new ArrayList<>();
             for(int i = 0; i < this.n; i++) reverseAdjList.add(new ArrayList<>());
 
             for(int i = 0; i < this.n; i++){
@@ -64,5 +68,51 @@ public class Graph {
         }
 
         return false;
+    }
+    
+    public void dfs1(int s){
+        this.visited[s] = 1;
+        ArrayList<Integer> adj = this.adjacencyList.get(s);
+        for(int i : adj){
+            if(this.visited[i] == 0) dfs1(i);
+        }
+        
+        myStack.push(s);
+    }
+    
+    public void dfs2(int s){
+        System.out.printf(s + " ");
+        this.visited[s] = 1;
+        ArrayList<Integer> adj = this.adjacencyList.get(s);
+        for(int i : adj){
+            if(this.visited[i] == 0) dfs2(i);
+        }
+    }
+    
+    public void stronglyConnectedComponent(){
+        this.visited = new int[this.n];
+        for(int i = 0; i < this.n; i++){
+            if(this.visited[i] == 0) dfs1(i);
+        }
+        
+        this.visited = new int[this.n];
+        this.reverseAdjList = new ArrayList<>();
+        for(int i = 0; i < this.n; i++) reverseAdjList.add(new ArrayList<>());
+
+        for(int i = 0; i < this.n; i++){
+            ArrayList<Integer> temp = this.adjacencyList.get(i);
+            for(int k : temp) reverseAdjList.get(k).add(i);
+        }
+        
+        int count = 1;
+        while(!myStack.empty()){
+            int node = myStack.peek();
+            myStack.pop();
+            if(this.visited[node] == 0){
+                System.out.println("SCC " + count++ + ":");
+                dfs2(node);
+                System.out.println();
+            }
+        }
     }
 }
