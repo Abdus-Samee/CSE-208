65dfd042ef9863b8fa613e0b2bb3e135849c4df7
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-01-17 17:23:19 +0600

    Create Node.java

---
+++ 5. Hashing/Node.java
@@ -0,0 +1,11 @@
+class Node{
+    String key;
+    int val;
+    Node next;
+
+    public Node(String key, int val){
+        this.key = key;
+        this.val = val;
+        this.next = null;
+    }
+}
